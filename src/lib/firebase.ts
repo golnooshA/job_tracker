8ca53b5d9e361b164b5e455fe839687bfb87f9d1
Golnoosh Asady <36160112+golnooshA@users.noreply.github.com>
2025-09-04 13:12:06 +0200

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -17,13 +17,13 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import type { Analytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAOvJSZQ0rtCHYlTmGESK4yZJTKYgsC-d4",
-  authDomain: "job-tracker-58689.firebaseapp.com",
-  projectId: "job-tracker-58689",
-  storageBucket: "job-tracker-58689.firebasestorage.app",
-  messagingSenderId: "197165158987",
-  appId: "1:197165158987:web:83e01b946271c2736cb3ce",
-  measurementId: "G-G76ZWBHFQK",
+  apiKey: "#############",
+  authDomain: "#############",
+  projectId: "#############",
+  storageBucket: "#############",
+  messagingSenderId: "#############",
+  appId: "#############",
+  measurementId: "#############",
 };
 
 // --- App (single instance) ---
